b93ed99c90311b7e3a4cd6d4307a0be1decf9910
konb-hui <2581881495@qq.com>
2019-09-07 11:42:55 +0800

    创建基础action类，继承ActionSupport和ModelDriver，实现getModel方法，添加跳转字符串和获取session的方法

---
+++ src/main/java/com/zph/supplychain/base/action/BaseAction.java
@@ -0,0 +1,47 @@
+package com.zph.supplychain.base.action;
+
+import java.lang.reflect.ParameterizedType;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class BaseAction<T> extends ActionSupport implements ModelDriven<T>{
+	
+	private Class classt;
+	private T t;
+	public BaseAction() {
+		ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
+		this.classt = (Class) type.getActualTypeArguments()[0];
+		try {
+			this.t = (T) this.classt.newInstance();
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	public T getModel() {
+		// TODO Auto-generated method stub
+		return this.t;
+	}
+
+	public static final String ADDUI = "addUI";//跳转到增加页面的字符串
+	public String addUI = ADDUI;
+	
+	public static final String UPDATEUI = "updateUI";//跳转到的修改页面的字符串
+	public String updateUI = UPDATEUI;
+	
+	public static final String LISTACTION = "listAction";//跳转到显示页面的字符串
+	public String listAction = LISTACTION;
+	
+	public static final String ACTION2ACTION = "action2action";//action跳转到action
+	public String action2action = ACTION2ACTION;
+	
+	public HttpSession getSession() {//获取session
+		return ServletActionContext.getRequest().getSession();
+	}
+}
