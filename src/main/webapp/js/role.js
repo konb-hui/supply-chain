fa9d0e719cf4ae58456b44d619f3379450fb8b3e
konb-hui <2581881495@qq.com>
2019-09-14 11:36:01 +0800

    role的右键菜单

--- src/main/webapp/js/role.js
+++ src/main/webapp/js/role.js
@@ -7,12 +7,24 @@ var roletree = {
 			root:{
 				isRoot:true,
 				nodes:{}
+			},
+			callback:{//回调函数
+				rightClick:function(event,treeId,treeNode){
+					roletree.showRMenu(event.clientX,event.clientY);
+				}
 			}
 		},
 		loadRoleTree:function(){
 			$.post("roleAction_showRoleTree.action",null,function(data){
 				$("#roleTree").zTree(roletree.setting,data);
 			});
+		},
+		/**
+		 * 显示右键菜单 
+		 */
+		showRMenu:function(x,y){
+			$("#rMenu ul").show();
+			$("#rMenu").css({"top":y+"px","left":x+"px","display":"block"});
 		}
 };
 $().ready(function(){
